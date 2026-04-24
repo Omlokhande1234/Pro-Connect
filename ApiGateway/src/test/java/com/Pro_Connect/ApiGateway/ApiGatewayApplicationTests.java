ce08e828c39c1cc3026bf18ac3eac1af4f4b8420
olokhande56@gmail.com <olokhande56@gmail.com>
2026-04-24 21:21:24 +0530

    feat: set up of ApiGateway service

---
+++ ApiGateway/src/test/java/com/Pro_Connect/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pro_Connect.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
